42c49558a11ff20d90a3d07db2edfac9094fe090
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2016-04-02 18:30:27 +0200

    Cosmetics

--- defuzzed.d
+++ defuzzed.d
@@ -12,6 +12,8 @@
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
+ *
+ * This file is part of defuzzed, a fuzzer for D compilers;
  */
 
 import std.array;
